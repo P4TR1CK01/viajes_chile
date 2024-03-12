8b545de425f3f6e354bdf93294fc5ae9711d3114
Patrick <patrick.ojeda.91@gmail.com>
2024-03-12 00:30:11 -0300

    function hide card´s added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 //ALERTA MENSAJE FORMULARIO//
 
 $('.btn-info').click(function(){
-  alert('Enviado')
+  alert('Mensaje enviado')
 })
 
 //FUNCION HIDE CARS//
 
 $('.card1').click(function(){
+  $('.card1').hide();
 });
+
+$('.card2').click(function(){
+  $('.card2').hide();
+});
+
+$('.card3').click(function(){
+  $('.card3').hide();
+});
+
+$('.card4').click(function(){
+  $('.card4').hide();
+});
+
